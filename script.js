ccad558750a616d0f7fef1cccd0fa77b5ba26553
Megan Coyle <mcoyle@usgbc.org>
2017-05-17 08:49:17 -0400

    Add XML data

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ if (window.XMLHttpRequest) {
 } else {
   request = new ActiveXObject("Microsoft.XMLHTTP");
 }
-request.open('GET', 'data.txt');
+request.open('GET', 'data.xml');
 request.onreadystatechange = function() {
 	if ((request.readyState===4) && (request.status===200)) {
-		var modify = document.getElementsByTagName('li');
-    modify[2].innerHTML = request.responseText;
-    for (var i=0; i < mdify.length; i++) {
-      modify[i].innerHTML = request.responseText;
+		console.log(request.responseXML.getElementsByTagName('name')[1].firstChild.nodeValue);
+
+    var items = request.responseXML.getElementsByTagName('name');
+    var output = '<ul>';
+    for (var i = 0; i < items.length; i++) {
+      output += '<li>' + items[i].firstChild.nodeValue + '</li>';
     }
+    output += '</ul>';
+    document.getElementById('update').innerHTML = output;
 	}
 }
 request.send();
