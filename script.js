13c743471d02ae57e22d63d9daf5fdf07345a151
Megan Coyle <mcoyle@usgbc.org>
2017-05-18 08:44:33 -0400

    Add jquery

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-var mybutton = document.getElementById('loadbutton');
-mybutton.onclick = loadAJAX;
-
-function loadAJAX() {
-  var request;
-  if (window.XMLHttpRequest) {
-    request = new XMLHttpRequest();
-  } else {
-    request = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  request.open('GET', 'data.json');
-  request.onreadystatechange = function() {
-  	if ((request.readyState===4) && (request.status===200)) {
-  		var items = JSON.parse(request.responseText);
-      var output = '<ul>';
-      for (var key in items) {
-        output += '<li>' + items[key].name + '</li>';
-      }
-      output += '</ul>';
-      document.getElementById('update').innerHTML = output;
-  	}
-  }
-  request.send();
-}
+$.getJSON('data.json', function(data) {
+  var output = '<ul>';
+  $.each(data, function(key, val) {
+    output += '<li>' + val.name + '</li>';
+  });
+  output += '</ul>';
+  $('#update').append(output);
+});
